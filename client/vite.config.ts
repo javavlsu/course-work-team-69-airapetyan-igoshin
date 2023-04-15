b81dacc262f43caec4f63e4c91233fc4e3777395
Denis <d.igoschin.igoshin@gmail.com>
2023-04-15 17:44:06 +0300

    hotfix: fix proxy rewrite param

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace('/api', ''),
+        rewrite: path => path.replace('/api', '/api'),
       }
     }
   }
