e97118af4308d374dcad4192e0d5960141ad0946
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2023-05-06 22:30:04 +0300

    Expand GET /post result by blogName and blogId

--- blogplatform/src/main/java/ru/theblog/blogplatform/api/controller/AppController.java
+++ blogplatform/src/main/java/ru/theblog/blogplatform/api/controller/AppController.java
@@ -180,7 +180,7 @@ public class AppController {
     @GetMapping("/post/{postId}")
     public PostResult getPost(@PathVariable long postId, Authentication auth) {
         var post = _postService.getPost(postId);
-        return new PostResult(post.getId(), post.getTitle(), post.getDescription(), post.getContent(), post.isDraft());
+        return new PostResult(post.getId(), post.getTitle(), post.getDescription(), post.getContent(), post.isDraft(), post.getBlog().getId(), post.getBlog().getName());
     }
 
     @PostMapping("/post")
--- blogplatform/src/main/java/ru/theblog/blogplatform/api/model/dto/PostResult.java
+++ blogplatform/src/main/java/ru/theblog/blogplatform/api/model/dto/PostResult.java
@@ -6,12 +6,16 @@ public class PostResult {
     public String description;
     public String content;
     public Boolean isDraft;
+    public Long blogId;
+    public String blogName;
 
-    public PostResult(long id, String title, String description, String content, Boolean isDraft) {
+    public PostResult(long id, String title, String description, String content, Boolean isDraft, long blogId, String blogName) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.content = content;
         this.isDraft = isDraft;
+        this.blogId = blogId;
+        this.blogName = blogName;
     }
 }
