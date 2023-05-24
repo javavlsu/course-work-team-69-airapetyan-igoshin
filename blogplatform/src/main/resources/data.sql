451262df5376dfd96045a7ac4d665ad12c60a788
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2023-05-24 11:01:26 +0300

    Add '/api/posts" method

--- blogplatform/src/main/resources/data.sql
+++ blogplatform/src/main/resources/data.sql
@@ -10,9 +10,9 @@ VALUES (DEFAULT, null, 'user@g.com', 'User', '$2a$10$xF5xxLpShRJSzBeniu704eLwpW3
         '2023-04-23', null, 1);
 
 INSERT INTO public.post (id, content, create_date, update_date, description, rating, title, blog_id, is_draft)
-VALUES (DEFAULT, 'Post1Draft', '2023-04-23 16:49:50.000000', '2023-04-23 16:49:50.000000', 'Text', 12, 'SamplePost', 1, true),
-       (DEFAULT, 'Post2Draft', '2023-04-23 16:49:50.000000', '2023-04-23 16:49:50.000000', 'Text', 12, 'SamplePost', 1, true),
-       (DEFAULT, 'Post3NotDraft', '2023-04-23 16:49:50.000000', '2023-04-23 16:49:50.000000', 'Text', 12, 'SamplePost', 1, false);
+VALUES (DEFAULT, 'Post1Draft', '2023-04-23 16:49:40.000000', '2023-04-23 16:49:40.000000', 'Text', 1128, 'SamplePost', 1, true),
+       (DEFAULT, 'Post2Draft', '2023-04-23 16:49:45.000000', '2023-04-23 16:49:45.000000', 'Text', 11, 'SamplePost', 1, true),
+       (DEFAULT, 'Post3NotDraft', '2023-04-23 16:49:50.000000', '2023-04-23 16:49:50.000000', 'Text', 15, 'SamplePost', 1, false);
 
 INSERT INTO public.user_blog_role (id, role, blog_id, user_id)
 VALUES (DEFAULT, 2, 1, 1);
